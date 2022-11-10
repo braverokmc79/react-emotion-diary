9159d2795bede416fcc719a69cd4ad2e3ac35fd3
braverokmc79 <braverokmc79@gmail.com>
2022-11-10 17:05:11 +0900

    53.페이지 라우팅 1 - React Router 기본

---
+++ src/components/RouteTest.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+
+const RouteTest = () => {
+    return (
+        <div>
+            <Link to={'/'} >HOME</Link>
+            <br />  <br />
+            <Link to={"/diary"}>DIARY</Link>
+            <br />  <br />
+            <Link to={"/new"}>NEW</Link>
+            <br />  <br />
+            <Link to={"/edit"}>EDIT</Link>
+        </div>
+    );
+};
+
+export default RouteTest;
---
+++ src/pages/New.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const New = () => {
+    return (
+        <div>
+            <h1>New</h1>
+            <p>이곳은 작성 페이지 입니다.</p>
+        </div>
+    );
+};
+
+export default React.memo(New);
